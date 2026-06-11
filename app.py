1c247f472386307d1ae93250eac8821f362893c9
SNXTH <sikdersamin71@gmail.com>
2026-06-11 06:09:02 +0600

    fix host binding

--- app.py
+++ app.py
@@ -138,5 +138,6 @@ def page_not_found(error: Exception) -> tuple[str, int]:
 
 
 if __name__ == '__main__':
+    import os
     ensure_database()
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
